cf1b6ba8cb3bfa1e24a8691f549e2ef433ae38be
bombaris34 <reapsgg@proton.me>
2025-06-02 15:37:05 +0200

    compile as lib

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 #include "iced++/iced.hpp"
 
-int main() {
+void iced_test() {
 	unsigned char code[] = { 0x55, 0x48, 0x89, 0xE5, 0x48, 0x83, 0xEC, 0x10, 0x48, 0x89, 0xC8, 0x48, 0x01, 0xD0, 0x4C, 0x01, 0xC0, 0x4C, 0x01, 0xC8, 0x48, 0xC7, 0xC1, 0x05, 0x00, 0x00, 0x00, 0x48, 0xF7, 0xE1, 0x48, 0x89, 0xEC, 0x5D, 0xC3 };
 	iced::Decoder<true> decoder{ code, sizeof(code), 0};
 	while (decoder.canDecode()) {
@@ -13,7 +13,7 @@ int main() {
 		std::cout << "\tOperand count: " << (int)instruction.operandCount() << std::endl;
 		for (auto i = 0u; i < instruction.operandCount(); ++i) {
 			auto op = instruction.operands;
-			std::cout << "\t\tOperand size: " << (int)op->size << std::endl;
+			std::cout << std::dec << "\t\t[" << i << "]Operand width : " << (int)op->width() << std::endl;
 		}
 		std::cout << "\tisMov: " << instruction.isMov() << std::endl;
 		std::cout << "\tisBranching: " << instruction.isBranching() << std::endl;
